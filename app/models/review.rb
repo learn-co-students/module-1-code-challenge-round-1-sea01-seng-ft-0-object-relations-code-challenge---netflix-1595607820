a12815cb3703712cbec56ecb30a657dfba0d9853
afteralec <github email address>
2020-07-24 09:46:46 -0700

    ment issues - just kidding, practice submission for code challenge 1 round 3

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
+  attr_reader :viewer, :movie
+  attr_accessor :rating
 
+  @@all = []
 
+  def initialize(viewer, movie, rating)
+    @viewer, @movie, @rating = viewer, movie, rating
+
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,9 +10,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+the_300 = Movie.new('300')
+v_for = Movie.new('V for Vendetta')
 
+soundarya = Viewer.new('Soundarya')
+ahmed = Viewer.new('Ahmed')
 
+soundarya.rate_movie(the_300, 3.7)
+soundarya.rate_movie(the_300, 4.3)
 
+ahmed.rate_movie(the_300, 5.0)
+ahmed.rate_movie(v_for, 4.3)
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
